f9d41f0a5c5848f0c36b327cd5da9eb756078b5f
ronitebul1211 <ronitebul1211@gmail.com>
2020-10-23 18:40:38 +0300

    display next question

--- src/components/QuestionField.js
+++ src/components/QuestionField.js
@@ -5,15 +5,14 @@ import "./QuestionField.css";
 /**
  Type 'date' -> content {label:'', value:''}  config{inputName:'', displayMode: 'row' / 'column'} 
  */
-const QuestionField = ({ selectedValue, config, onChangeCallback, question, answers }) => {
-   const renderedRadioOptions = answers.map((answer) => {
+const QuestionField = ({ questionData, selectedValue, onChangeCallback }) => {
+   const renderedRadioOptions = questionData.answers.map((answer) => {
       return (
          <div className="answer" key={answer.answerId}>
             <input
                className="answer__input"
                value={answer.answerId}
                type="radio"
-               name={"question num"}
                checked={answer.answerId === selectedValue}
                onChange={onChangeCallback}
             />
@@ -24,7 +23,7 @@ const QuestionField = ({ selectedValue, config, onChangeCallback, question, answ
 
    return (
       <div className="question-field">
-         <label className="question-field__question-text">{question}</label>
+         <label className="question-field__question-text">{questionData.question.text}</label>
          <div className="question-field__answers">{renderedRadioOptions}</div>
       </div>
    );
--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -3,40 +3,67 @@ import quizApi from "../api/quizApi";
 import QuestionField from "../components/QuestionField";
 
 class QuizPage extends React.Component {
-   state = { currentQuestion: {}, currentAnswers: [], currentIndex: 0 };
+   state = { quizData: [], currentQuestionIndex: 0, currentAnswerId: 1, quizResults: [] };
 
    async componentDidMount() {
-      this.quiz = await quizApi.getQuizData();
-      console.log(this.quiz);
-
-      this.setState(
-         {
-            currentQuestion: this.quiz[this.state.currentIndex].question,
-            currentAnswers: this.quiz[this.state.currentIndex].answers,
-         },
-         () => console.log(this.state),
-      );
+      const quizData = await quizApi.getQuizData();
+      this.setState({ quizData });
       //TODO catch network error and update UI
    }
 
+   onAnswerChange = (event) => {
+      const answerId = parseInt(event.target.value);
+      this.setState({ currentAnswerId: answerId }, () => console.log(this.state));
+   };
+
+   onContinueClick = () => {
+      //Check is it last?
+
+      if (this.props.mode === "newUser") {
+         this.saveQuizResult();
+         if (!this.isQuizFinished()) {
+            this.renderNextQuestion();
+         } else {
+         }
+      }
+   };
+
+   renderNextQuestion = () => {
+      const nextQuestionIndex = this.state.currentQuestionIndex + 1;
+      this.setState({ currentQuestionIndex: nextQuestionIndex, currentAnswerId: 1 });
+   };
+   saveQuizResult = () => {
+      const quizResultsCopy = [...this.state.quizResults];
+      const questionId = this.getCurrentQuestionId();
+      const answerId = this.state.currentAnswerId;
+      quizResultsCopy.push({ questionId, answerId });
+      this.setState({ quizResults: quizResultsCopy });
+   };
+   getCurrentQuestionId = () => {
+      return this.state.quizData[this.state.currentQuestionIndex].question.id;
+   };
+   isQuizFinished = () => {
+      return this.state.currentQuestionIndex === this.state.quizData.length - 1;
+   };
+
    render() {
-      console.log(this.props.mode);
-      return (
-         <div>
-            QuizPage
-            <QuestionField
-               selectedValue={1}
-               question={this.state.currentQuestion.text}
-               answers={this.state.currentAnswers}
-               config={{
-                  inputName: "type",
-                  defaultValue: "חובה",
-                  displayMode: { field: "column", options: "row" },
-               }}
-               onChangeCallback={this.onChange}
-            />
-         </div>
-      );
+      let questionUi;
+      if (this.state.quizData.length > 0) {
+         questionUi = (
+            <React.Fragment>
+               <span>שאלה מס' {this.state.currentQuestionIndex + 1}</span>
+               <QuestionField
+                  selectedValue={this.state.currentAnswerId}
+                  questionData={this.state.quizData[this.state.currentQuestionIndex]}
+                  onChangeCallback={this.onAnswerChange}
+               />
+               <button onClick={this.onContinueClick}>המשך</button>
+            </React.Fragment>
+         );
+      } else {
+         questionUi = <React.Fragment>בטעינה...</React.Fragment>;
+      }
+      return <div>{questionUi}</div>;
    }
 }
 
